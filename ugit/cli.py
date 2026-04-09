07cfd14b2a6e2aa5211d60f4d1207aef819f5997
Rafael Lopez <rafael.lopez.172@my.csun.edu>
2026-04-08 21:40:49 -0700

    Added hash_objects and cat_file commands

--- ugit/cli.py
+++ ugit/cli.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import sys
 
 from . import data
 
@@ -8,15 +9,15 @@ def main():
     args.func(args)
 
 
-def parse_args()):
+def parse_args():
 	#Creates new argument parser
     parser = argparse.ArgumentParser()
 
-    #Must provide subcommans like 'init'
+    #Must provide subcommands like 'init'
     commands = parser.add_subparsers(dest='command')
     commands.required = True
 
-    #Checking for understanding
+    #Practice: Checking for understanding
     clone_parser = commands.add_parser('clone')
     clone_parser.set_defaults(func=clone)
 
@@ -25,12 +26,32 @@ def parse_args()):
     #Links 'init' command to def init(args)
     init_parser.set_defaults(func=init)
 
+    #Makes the object/file into hash1
+    hash_object_parser = commands.add_parser('hash-object')
+    hash_object_parser.set_defaults(func=hash_object)
+    hash_object_parser.add_argument('file')
+
+    #Returns the object's/file's contents
+    cat_file_parser = commands.add_parser('cat-file')
+    cat_file_parser.set_defaults(func=cat_file)
+    cat_file_parser.add_argument('object')
+
     return parser.parse_args()
 
-
-def init (args):
+def init(args):
     data.init()
     print(f'Initialized empty ugit repository in {os.getcwd()}/{data.GIT_DIR}')
 
-def clone (args):
+def clone(args):
 	print('Hello from clone')
+
+def hash_object(args):
+    with open(args.file, 'rb') as f:
+        print(data.hash_object(f.read()))
+
+def cat_file(args):
+    #Immediately writes out everything in the buffer 
+    sys.stdout.flush()
+    sys.stdout.buffer.write(data.get_object(args.object))
+
+
--- ugit/data.py
+++ ugit/data.py
@@ -1,6 +1,19 @@
+import hashlib
 import os
 
 GIT_DIR = '.ugit'
 
 def init():
-	os.makdirs(GIT_DIR)
+	os.makedirs(GIT_DIR)
+	os.makedirs(f'{GIT_DIR}/objects')
+
+
+def hash_object(data):
+	oid = hashlib.sha1(data).hexdigest()
+	with open(f'{GIT_DIR}/objects/{oid}', 'wb') as out:
+		out.write(data)
+	return oid
+
+def get_object(oid):
+	with open(f'{GIT_DIR}/objects/{oid}', 'rb') as f:
+		return f.read()
